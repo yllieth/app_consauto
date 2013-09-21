b40691a1400f4665f6bda00620509b8ef2c862c4
yllieth <sylvnimes@hotmail.com>
2013-09-21 15:06:34 +0200

    [ACCUEIL] boutons sur la page d'accueil vers les différentes activities

--- Consauto/src/main/java/syl/consauto/app/MainActivity.java
+++ Consauto/src/main/java/syl/consauto/app/MainActivity.java
@@ -5,6 +5,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
@@ -54,4 +56,15 @@ public class MainActivity extends Activity {
         return false;
     }
 
+    public void startFaireLePleinActivity(View view) {
+        startActivity(new Intent(view.getContext(), FaireLePleinActivity.class));
+    }
+
+    public void startListeActivity(View view) {
+        startActivity(new Intent(view.getContext(), ListeActivity.class));
+    }
+
+    public void startSettingsActivity(View view) {
+        startActivity(new Intent(view.getContext(), SettingsActivity.class));
+    }
 }
